20d01d28888c2b4e92990e86f58e7a077b21783f
sim <sim@DESKTOP-JFTIL1T>
2019-11-14 15:00:39 +0900

    스킬 구현 hp bar 조각냄

---
+++ src/game/stage3/run/Run.java
@@ -0,0 +1,11 @@
+package game.stage3.run;
+
+import game.stage3.views.Stage3Frame;
+public class Run {
+
+	public static void main(String[] args) {
+		new Stage3Frame();
+
+	}
+
+}
--- src/game/stage3/stage3.java
+++
@@ -1,8 +0,0 @@
-package game.stage3;
-
-public class stage3 {
-	private void jaewoo() {
-		System.out.println("재우입니다 ");
-
-	}
-}
