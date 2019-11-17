780c02acfd5a5379349e3fed9d342f8cd0576242
사홍 <@DESKTOP-GR99B2N>
2019-11-18 08:49:42 +0900

    11/18 오전업데이트

--- src/game/stage2/view/E_SuccessPanel.java
+++ src/game/stage2/view/E_SuccessPanel.java
@@ -5,21 +5,40 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class E_SuccessPanel extends JPanel{
+import view.C_GameStage;
+
+public class E_SuccessPanel extends Thread{
 	private JFrame mf;
 	private JPanel panel;
 	
-	public E_SuccessPanel(JFrame mf) {
+	public E_SuccessPanel(JFrame mf, JPanel panel) {
 		
 		this.mf = mf;
-		panel = this;
+		this.panel = panel;
 		
-		JLabel suc = new JLabel(new ImageIcon(new ImageIcon("images/stage2/stage2_suss.gif").
-				getImage().getScaledInstance(1280, 720, 0)));
-		
-		this.add(suc, "Center");
 		
 	}
+	@Override
+	public void run() {
+		//성공이미지 출력
+		JLabel suc = new JLabel(new ImageIcon(new ImageIcon("images/stage2/stage2_suss.gif").
+				getImage().getScaledInstance(1280, 720, 0)));
+		//위치
+		panel.add(suc, "Center");
+		
+		try {
+			this.sleep(1000);
+			System.out.println("시간가는중");
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+		
+	}
+	//타이머가 끝나면 다음 메인화면으로 넘어감
+	E_ChangePanel cp = new E_ChangePanel(mf, panel);
+	C_GameStage gs = new C_GameStage(mf);
+	
 	
 	
 }
