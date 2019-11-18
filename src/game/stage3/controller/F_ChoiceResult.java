3291c03a1469b46ab94f9abf3c42240fedb9f98b
sim <sim@DESKTOP-JFTIL1T>
2019-11-18 14:57:07 +0900

    뷰패널에서 룰 구현중

---
+++ src/game/stage3/controller/F_ChoiceResult.java
@@ -0,0 +1,18 @@
+package game.stage3.controller;
+
+public class F_ChoiceResult {
+	private int choice;
+
+	public F_ChoiceResult() {}
+
+	public int getChoice() {
+		return choice;
+	}
+
+	public void setChoice(int choice) {
+		this.choice = choice;
+	}
+	
+	
+	
+}
