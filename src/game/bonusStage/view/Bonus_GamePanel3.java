45a59119af45d7f2da8a7eac560f809b2e33b1cd
hayoungpark <hayoungpark@192.168.31.90>
2019-11-21 14:58:18 +0900

    bonus game!!

--- src/game/bonusStage/view/Bonus_GamePanel3.java
+++ src/game/bonusStage/view/Bonus_GamePanel3.java
@@ -43,25 +43,45 @@ public class Bonus_GamePanel3 extends JPanel {
 				hokHp--;
 				System.out.println("호크룩스 HP" + hokHp);
 				
-				if (ctn > 10 && ctn < 15 ) {
-					imgNum = 2;
-					System.out.println(imgNum);
-				} else if ( ctn >= 15) {
-					imgNum = 1;
+				if( ctn < 5) {
+					 hok1.setIcon(new ImageIcon("images/bonus/hok1.png"));
 				}
+				if( ctn == 5 ) {
+					hok1.setIcon(new ImageIcon("images/bonus/hok2.png"));
+				} else if( ctn == 10 ) {
+					hok1.setIcon(new ImageIcon("images/bonus/hok3.png"));
+				} else if( ctn == 15 ) {
+					hok1.setIcon(new ImageIcon("images/bonus/hok4.png"));
+				} else if( ctn == 20 ) {
+					hok1.setIcon(new ImageIcon("images/bonus/hok5.png"));
+				} else if( ctn == 25 ) {
+					hok1.setIcon(new ImageIcon("images/bonus/hok6.png"));
+				} else if( ctn == 30 ) {
+					hok1.setIcon(new ImageIcon("images/bonus/hok7.png"));
+				} else if (ctn > 35){
+					 hok1.setIcon(new ImageIcon("images/bonus/hokLast3.gif"));
+				}
+				
+				
+//				if (ctn > 10 && ctn < 15 ) {
+//					imgNum = 2;
+//					System.out.println(imgNum);
+//				} else if ( ctn >= 15) {
+//					imgNum = 1;
+//					 hok1.setIcon(new ImageIcon("images/bonus/hokLast2.gif"));
+//				}
 				
 				
 				
 				currLocation = mf.getLocationOnScreen();
 
-			    Point position1 = new Point(currLocation.x + 5, currLocation.y);
-			    Point position2 = new Point(currLocation.x - 5, currLocation.y);
-			    for (int i = 0; i < 20; i++) {
+			    Point position1 = new Point(currLocation.x + 4, currLocation.y);
+			    Point position2 = new Point(currLocation.x - 4, currLocation.y);
+			    for (int i = 0; i < 5; i++) {
 			    	mf.setLocation(position1);
 			    	mf.setLocation(position2);
 			    }
 			    mf.setLocation(currLocation);
-			    hok1.setIcon(new ImageIcon("images/bonus/hok" + imgNum + ".png"));
 			  }
 			
 			
